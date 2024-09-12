c674e094a7b96a2f250d238b01d7d1f6eb6742e4
Kevin <kvilleda10566@gmail.com>
2024-09-12 10:02:38 -0400

    Done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,27 @@
 import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import NavBar from "../components/NavBar";
 
 function Home() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/movies") // Assuming movies are stored on your json-server
+      .then((res) => res.json())
+      .then((data) => setMovies(data));
+  }, []);
+
   return (
     <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
+      <NavBar />
+      <h1>Home Page</h1>
+      <div>
+        {movies.map(movie => (
+          <MovieCard key={movie.id} title={movie.title} id={movie.id} />
+        ))}
+      </div>
     </>
   );
-};
+}
 
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,27 @@
-
+import Home from './pages/Home';
+import Directors from './pages/Directors';
+import Actors from './pages/Actors';
+import Movie from './pages/Movie';
+import ErrorPage from './pages/ErrorPage';
 
 const routes = [
-   
-  ];
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/directors",
+    element: <Directors />
+  },
+  {
+    path: "/actors",
+    element: <Actors />
+  },
+  {
+    path: "/movie/:id", // Dynamic route with id as a parameter
+    element: <Movie />
+  }
+];
 
 export default routes;
